398ff5063b8da1d942b9737eeb42c881f673e0a2
YeetDeckx <deckx.piet@gmail.com>
2020-05-19 09:50:40 +0200

    map naam aanspassen

--- Monopoly_Model/Properties/AssemblyInfo.cs
+++ Monopoly_Model/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Monopoly_Models")]
+[assembly: AssemblyTitle("Monopoly_Model")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Monopoly_Models")]
+[assembly: AssemblyProduct("Monopoly_Model")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
