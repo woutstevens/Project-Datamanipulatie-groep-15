6140b7a6197c451e781ff0af0d0c7b9e07572e26
woutstevens <r0744397@student.thomasmore.be>
2020-05-24 16:46:30 +0200

    Scherm AlgemeenFonds

--- Project_Monopoly/AlgemeenFonds.xaml.cs
+++ Project_Monopoly/AlgemeenFonds.xaml.cs
@@ -28,5 +28,10 @@ namespace Project_Monopoly
         {
 
         }
+
+        private void btnDoorgaan_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
